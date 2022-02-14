59abde65d1a768c097987633f89dec8424f641ad
jamalbutt12322 <jamal.butt@wecrunch.com>
2022-02-15 01:52:25 +0500

    register crash issue fixed

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -5,39 +5,38 @@ const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   try {
     // Check if a user already exists
-    await User.findOne({ email: req.body.email }).then((user) => {
-      if (user) {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const result = {
+        status_code: 500,
+        status_msg: `This user is already registered with ${req.body.email} e-mail`,
+      };
+      res.status(500).send(result);
+    } else {
+      if (req.body.password === req.body.confirmpassword) {
+        //   generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        // create new user
+        const newUser = new User({
+          email: req.body.email,
+          password: hashedPassword,
+        });
+        // save user and responsd
+        const user = await newUser.save();
+        const result = {
+          status_code: 200,
+          status_msg: "User successfully registered",
+          data: user,
+        };
+        res.status(200).send(result);
+      } else {
         const result = {
           status_code: 500,
-          status_msg: `This user is already registered with ${req.body.email} e-mail`,
+          status_msg: "Both password fields does not match",
         };
         res.status(500).send(result);
       }
-    });
-
-    if (req.body.password === req.body.confirmpassword) {
-      //   generate new password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(req.body.password, salt);
-      // create new user
-      const newUser = new User({
-        email: req.body.email,
-        password: hashedPassword,
-      });
-      // save user and responsd
-      const user = await newUser.save();
-      const result = {
-        status_code: 200,
-        status_msg: "User successfully registered",
-        data: user,
-      };
-      res.status(200).send(result);
-    } else {
-      const result = {
-        status_code: 500,
-        status_msg: "Both password fields does not match",
-      };
-      res.status(500).send(result);
     }
   } catch (err) {
     const result = {
